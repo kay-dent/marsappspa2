7850fe32d9ad3febaa5517b29c3a5f7ec21baa3e
Kay Dent <Kay.Dent@softwire.com>
2024-04-29 14:41:18 +0100

    Replaced image and text to be about NASA

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,12 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          NASA hey there
-        </p>
+        <img src="https://www.nasa.gov/wp-content/themes/nasa/assets/images/nasa-logo.svg" className="App-logo" alt="logo" />
+        <div>
+          <h1>NASA</h1>
+          <p>Nasa is a space agency in the US.</p>
+          <p>Nasa's satellites provide data about Earth.</p>
+        </div>
         <a
           className="App-link"
           href="https://reactjs.org"
